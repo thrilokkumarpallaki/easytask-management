983eca03839efc4b3afe62871813de98e446c0ca
Thrilok Kumar Pallaki <thrilokkumarpallaki@gmail.com>
2024-08-07 12:50:23 -0600

    Removed dead code from the user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,8 @@
-import { Component, computed, EventEmitter, Input, input, Output, signal, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { type User } from './user.model';
-import { DUMMY_USERS } from '../dummy-users';
 import { CardComponent } from '../shared/card/card.component';
 
-const randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -17,20 +15,9 @@ export class UserComponent {
   // Decorator based inputs
   @Input({required: true}) user!: User;
   @Input({required: true}) selected!: boolean;
-  // @Input({required: true}) id!: string;
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-
-  // Sigal based inputs - Angular 16 feature
-  // id = input.required<string>();
-  // avatar = input.required<string>();
-  // name = input.required<string>();
 
   // Decorator based output 
   @Output() select = new EventEmitter<string>();
-
-  // Singal based output - Angular 16 feature
-  // select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
